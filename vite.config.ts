c873c26ab3c5c3202507e63160805f7af0749808
Gaurav <gourav.oppo123@gmail.com>
2026-06-14 19:43:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Restoring the explicit case-sensitive repository path
+  // Binds asset generation directly to your case-sensitive repo name
   base: '/Axion/',
   plugins: [react()],
   server: {
